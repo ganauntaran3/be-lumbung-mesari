dabd65dd92885ef09f355fa97c5faf51259d1dd7
I Gusti Ngurah Gana Untaran <i.gusti.n.g.untaran@gdplabs.id>
2025-08-03 11:45:00 +0700

    [Feature] API Documentation (#16)
    * update: setup swagger documentation
    
    * update: enable multiple version instance running
    
    * update: checklist for API docs

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,45 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from '@nestjs/core'
+import { AppModule } from './app.module'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
+import helmet from 'helmet'
+import * as compression from 'compression'
+import { VersioningType } from '@nestjs/common'
+import { ConfigService } from '@nestjs/config'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(8000);
+  const app = await NestFactory.create(AppModule)
+  const configService = app.get(ConfigService)
+  const apiVersion = configService.get<string>('API_DEFAULT_VERSION')
+  const enabledVersions = configService.get<string>('API_ENABLED_VERSION')
+
+  // Security middleware
+  app.use(helmet())
+  app.use(compression())
+  app.enableCors()
+
+  // Global prefix
+  app.setGlobalPrefix('api')
+
+  // Versioning
+  enabledVersions.split(',').forEach((version) => {
+    app.enableVersioning({
+      type: VersioningType.URI,
+      defaultVersion: apiVersion
+    })
+
+    // Swagger configuration
+    const config = new DocumentBuilder()
+      .setTitle('Lumbung Mesari API')
+      .setDescription('The Lumbung Mesari API documentation')
+      .setVersion(`${version}`)
+      .addBearerAuth()
+      .build()
+
+    const document = SwaggerModule.createDocument(app, config)
+    SwaggerModule.setup(`api/v${version}`, app, document)
+  })
+
+  const port = configService.get<number>('PORT', 8000)
+  await app.listen(port)
 }
-bootstrap();
+bootstrap()
---
+++ src/users/users.controller.ts
@@ -0,0 +1,32 @@
+import {
+  Controller,
+  Get,
+  Inject,
+  Version,
+  VERSION_NEUTRAL
+} from '@nestjs/common'
+import { UsersService } from './users.service'
+
+@Controller('/users')
+export class UsersController {
+  @Inject()
+  private readonly usersService: UsersService
+
+  @Version(VERSION_NEUTRAL)
+  @Get()
+  async findAllNeutral() {
+    return this.findAllV1()
+  }
+
+  @Version('1')
+  @Get()
+  async findAllV1() {
+    return this.usersService.findAll()
+  }
+
+  @Version('2')
+  @Get()
+  async findAllV2() {
+    return this.usersService.findAll()
+  }
+}
