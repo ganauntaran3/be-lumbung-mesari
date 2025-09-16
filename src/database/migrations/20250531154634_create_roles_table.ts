4b2284c7aee57f13ab906d74ccdc3f8ce49ba72b
I Gusti Ngurah Gana Untaran <i.gusti.n.g.untaran@gdplabs.id>
2025-09-16 21:06:40 +0700

    update: fix uuidv7 integration

--- src/database/migrations/20250531154634_create_roles_table.ts
+++ src/database/migrations/20250531154634_create_roles_table.ts
@@ -1,7 +1,7 @@
 import { Knex } from 'knex'
 
 export async function up(knex: Knex): Promise<void> {
-  await knex.raw('CREATE EXTENSION IF NOT EXISTS "pg_uuidv7";')
+  await knex.raw('CREATE EXTENSION IF NOT EXISTS pg_uuidv7;')
 
   await knex.schema.createTable('roles', (table) => {
     table.uuid('id').primary().defaultTo(knex.raw('uuid_generate_v7()'))
